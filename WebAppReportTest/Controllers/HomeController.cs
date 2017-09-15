88d4198ccbd0b9ac6704dd4d75b9dc661d0f3e7f
fernanda barba <ma.fer.bv23@gmail.com>
2017-09-15 13:45:30 -0500

    Se agrega un ejemplo de reporte para mostrar. Se agrega modelo y dataSet.

--- WebAppReportTest/Controllers/HomeController.cs
+++ WebAppReportTest/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Microsoft.Reporting.WebForms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebAppReportTest.Models;
 
 namespace WebAppReportTest.Controllers
 {
@@ -13,18 +15,84 @@ namespace WebAppReportTest.Controllers
             return View();
         }
 
-        public ActionResult About()
+        [AllowAnonymous]
+        public ActionResult ReportViewer(string returnUrl)
         {
-            ViewBag.Message = "Your application description page.";
-
+            ViewBag.ReturnUrl = returnUrl;
             return View();
         }
 
-        public ActionResult Contact()
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ViewResult ReportViewer(SearchParameterModel um)
         {
-            ViewBag.Message = "Your contact page.";
+            return View("Index", um);
+        }
 
-            return View();
+
+        public FileContentResult GenerateAndDisplayReport(string territory, string format)
+        {
+            LocalReport localReport = new LocalReport();
+            localReport.ReportPath = Server.MapPath("~/Content/Report1.rdlc");
+
+            ReportDataSource reportDataSource = new ReportDataSource();
+            reportDataSource.Name = "DataSet1";
+            using (Entities1 db = new Entities1())
+            {
+                if (territory != null)
+                    reportDataSource.Value = db.mUsers.Where(u => u.UserID.ToString() == territory).ToList();
+                else
+                    reportDataSource.Value = db.mUsers.ToList();
+            }
+
+            localReport.DataSources.Add(reportDataSource);
+
+            ReportDataSource reportDataSource2 = new ReportDataSource();
+            reportDataSource2.Name = "DataSet2";
+            using (Entities1 db = new Entities1())
+            {
+                reportDataSource2.Value = db.mOperations.ToList();
+            }
+            
+            localReport.DataSources.Add(reportDataSource2);
+
+            string reportType = "Image";
+            string mimeType;
+            string encoding;
+            string fileNameExtension;
+
+            Warning[] warnings;
+            string[] streams;
+            byte[] renderedBytes;
+            //Render the report
+
+            if (format == null)
+            {
+                renderedBytes = localReport.Render(reportType, null, out mimeType, out encoding, out fileNameExtension, out streams, out warnings);
+                Response.AddHeader("content-disposition", "attachment; filename=NorthWindCustomers." + fileNameExtension);
+
+                return File(renderedBytes, "image/jpeg");
+            }
+            else if (format.ToUpper() == "PDF")
+            {
+                renderedBytes = localReport.Render("pdf", null, out mimeType, out encoding, out fileNameExtension, out streams, out warnings);
+                Response.AddHeader("content-disposition", "attachment; filename=NorthWindCustomers.pdf");
+
+                return File(renderedBytes, "pdf");
+            }
+            else if (format.ToUpper() == "EXCEL")
+            {
+                renderedBytes = localReport.Render("Excel", null, out mimeType, out encoding, out fileNameExtension, out streams, out warnings);
+                Response.AddHeader("content-disposition", "attachment; filename=NorthWindCustomers.xls");
+
+                return File(renderedBytes, "application/vnd.ms-excel");
+            }
+            else
+            {
+                renderedBytes = localReport.Render(reportType, null, out mimeType, out encoding, out fileNameExtension, out streams, out warnings);
+                return File(renderedBytes, "image/jpeg");
+            }
         }
     }
 }
---
+++ WebAppReportTest/Models/SearchParameterModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebAppReportTest.Models
+{
+    public class SearchParameterModel
+    {
+
+        [Display(Name = "Buscar por Id")]
+        public string Id
+        {
+            get;
+
+            set;
+        }
+        public string Format
+        {
+            get;
+
+            set;
+        }
+    }
+}
